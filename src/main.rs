525a7f0594076cca25b26f99d1f3515ac997053f
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-05-27 01:07:32 +0300

    feat: integrate with mousefood

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,118 @@ mod input;
 mod transform;
 mod ui;
 
-use ratatui::crossterm::event::{self, Event, KeyCode};
+use std::error::Error;
+use std::thread;
+
+use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
+use mousefood::{prelude::*};
+
+use embedded_hal::spi::MODE_3;
+
+use esp_idf_svc::hal::{
+    delay::Ets,
+    gpio::{AnyIOPin, PinDriver},
+    peripherals::Peripherals,
+    spi::{SpiConfig, SpiDeviceDriver, SpiDriverConfig},
+    units::*,
+};
+use mipidsi::{interface::SpiInterface, models::ST7789, Builder};
+
+use mipidsi::options::ColorInversion;
+
 use std::sync::mpsc;
 use transform::Transform;
 use ui::*;
 
-const SAMPLES: &str = include_str!("samples.txt");
-
 fn mock_data(tx: mpsc::Sender<Vec<i16>>) {
-    // Parse the string into a Vec<i16>
-    let parsed_samples: Vec<i16> = SAMPLES
-        .trim_matches(&['[', ']'][..])
-        .split(',')
-        .filter_map(|s| s.trim().parse().ok())
-        .collect();
+    let builder = thread::Builder::new().name("mock_data_thread".into()).stack_size(10 * 1024); // 64KB stack
 
-    std::thread::spawn(move || {
-        let mut index = 0;
+    builder
+        .spawn(move || {
+            let mut samples = vec![0i16; 10]; // smaller buffer
+            let mut i = 0;
 
-        loop {
-            // Create a 1024-sample chunk from parsed_samples, wrapping if needed
-            let chunk: Vec<i16> = (0..1024)
-                .map(|i| {
-                    let sample_index = (index + i) % parsed_samples.len();
-                    parsed_samples[sample_index]
-                })
-                .collect();
+            loop {
+                for sample in &mut samples {
+                    *sample = (i % 32768) as i16;
+                    i += 1;
+                }
 
-            if tx.send(chunk).is_err() {
-                break; // Exit if receiver is gone
+                if tx.send(samples.clone()).is_err() {
+                    break;
+                }
+
+                thread::sleep(std::time::Duration::from_millis(10));
             }
-
-            index = (index + 1024) % parsed_samples.len(); // Advance position
-
-            std::thread::sleep(std::time::Duration::from_millis(10));
-        }
-    });
+        })
+        .expect("Failed to spawn mock data thread with custom stack size");
 }
 
-fn main() {
-    let (tx, rx) = mpsc::channel::<Vec<i16>>();
+const DISPLAY_OFFSET: (u16, u16) = (52, 40);
+const DISPLAY_SIZE: (u16, u16) = (135, 240);
 
-    // let recorder = input::Recorder::init("pipewire", move |data: &[i16], _| {
-    //     tx.send(data.to_vec()).unwrap();
-    // });
-    // recorder.start();
+fn main() -> Result<(), Box<dyn Error>> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take()?;
+
+    // Turn on display backlight
+    let mut backlight = PinDriver::output(peripherals.pins.gpio4)?;
+    backlight.set_high()?;
+
+    // Configure SPI
+    let config = SpiConfig::new()
+        .write_only(true)
+        .baudrate(80u32.MHz().into())
+        .data_mode(MODE_3);
+    let spi_device = SpiDeviceDriver::new_single(
+        peripherals.spi2,
+        peripherals.pins.gpio18,
+        peripherals.pins.gpio19,
+        Option::<AnyIOPin>::None,
+        Some(peripherals.pins.gpio5),
+        &SpiDriverConfig::new(),
+        &config,
+    )?;
+    let buffer: &'static mut [u8; 512] = Box::leak(Box::new([0u8; 512]));
+    let spi_interface = SpiInterface::new(
+        spi_device,
+        PinDriver::output(peripherals.pins.gpio16)?,
+        buffer,
+    );
+
+    // Configure display
+    let mut delay = Ets;
+    let mut display = Builder::new(ST7789, spi_interface)
+        .invert_colors(ColorInversion::Inverted)
+        .reset_pin(PinDriver::output(peripherals.pins.gpio23)?)
+        .display_offset(DISPLAY_OFFSET.0, DISPLAY_OFFSET.1)
+        .display_size(DISPLAY_SIZE.0, DISPLAY_SIZE.1)
+        .init(&mut delay)
+        .expect("Failed to init display");
+
+    // Reset pixels
+    display
+        .clear(Rgb565::BLACK)
+        .expect("Failed to clear display");
+
+    let (tx, rx) = mpsc::channel::<Vec<i16>>();
 
     mock_data(tx);
 
     let sample_rate = 384000.;
-    let mut terminal = ratatui::init();
     let mut samples = Vec::new();
     let mut mode = 0;
 
+    let backend = EmbeddedBackend::new(&mut display, EmbeddedBackendConfig::default());
+    let mut terminal = Terminal::new(backend)?;
     loop {
         terminal
             .draw(|frame| {
@@ -79,16 +138,5 @@ fn main() {
             samples = v;
         }
 
-        if event::poll(std::time::Duration::from_millis(16)).unwrap() {
-            if let Event::Key(key) = event::read().unwrap() {
-                match key.code {
-                    KeyCode::Tab => {
-                        mode = (mode + 1) % 3;
-                    }
-                    _ => break,
-                }
-            }
-        }
     }
-    ratatui::restore();
 }
