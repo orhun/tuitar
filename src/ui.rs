d9695bd58074de81e0590d9dbdaa9988bc8cbf09
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-06-10 23:40:20 +0300

    fix: use the correct scaling and sample rate for waveform chart

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,5 @@
 use pitchy::Note;
-use ratatui::layout::{Constraint, Flex, Layout};
+use ratatui::layout::Offset;
 use ratatui::widgets::Block;
 use ratatui::Frame;
 use ratatui::{
@@ -12,15 +12,39 @@ use tui_big_text::{BigText, PixelSize};
 
 use crate::transform::Transform;
 
-pub fn draw_waveform(frame: &mut Frame<'_>, samples: &[i16]) {
+pub fn draw_waveform(frame: &mut Frame<'_>, samples: &[i16], sample_rate: f64, bounds: (f64, f64)) {
+    let duration = samples.len() as f64 / sample_rate;
     let data_points: Vec<(f64, f64)> = samples
         .iter()
         .enumerate()
         .map(|(i, &sample)| {
-            let time_in_seconds = (i as f64) / 16384.;
+            let time_in_seconds = (i as f64) / sample_rate as f64;
             (time_in_seconds, sample as f64)
         })
         .collect();
+
+    let label_count = 5;
+    let x_labels: Vec<String> = (0..label_count)
+        .map(|i| format!("{:.2}", i as f64 * duration / (label_count - 1) as f64))
+        .collect();
+
+    let x_axis = Axis::default()
+        .title("Time(s)".red())
+        .style(Style::default().white())
+        .bounds([0.0, duration])
+        .labels(x_labels);
+
+    let y_axis = Axis::default()
+        .title("Amplitude".red())
+        .style(Style::default().white())
+        .bounds([bounds.0, bounds.1])
+        .labels(vec![
+            format!("{:.0}", bounds.0),
+            format!("{:.0}", bounds.0 + (bounds.1 - bounds.0) * 0.25),
+            format!("{:.0}", bounds.0 + (bounds.1 - bounds.0) * 0.5),
+            format!("{:.0}", bounds.0 + (bounds.1 - bounds.0) * 0.75),
+            format!("{:.0}", bounds.1),
+        ]);
 
     let dataset = Dataset::default()
         .name("Audio Waveform")
@@ -28,18 +52,6 @@ pub fn draw_waveform(frame: &mut Frame<'_>, samples: &[i16]) {
         .graph_type(GraphType::Scatter)
         .style(Style::default().white())
         .data(&data_points);
-
-    let x_axis = Axis::default()
-        .title("Time(s)".red())
-        .style(Style::default().white())
-        .bounds([0.0, data_points.last().map(|v| v.0).unwrap_or_default()])
-        .labels(vec!["0", "1", "2", "3", "4"]);
-
-    let y_axis = Axis::default()
-        .title("Amplitude".red())
-        .style(Style::default().white())
-        .bounds([-32768.0, 32767.0])
-        .labels(vec!["-32768", "-16384", "0", "16384", "32767"]);
 
     let chart = Chart::new(vec![dataset]).x_axis(x_axis).y_axis(y_axis);
 
