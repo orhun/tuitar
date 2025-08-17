b212625656672736d363e9348eafbdd166e23dc9
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-08-18 01:46:50 +0300

    fix(firmware): use the correct parameter for random note generation

--- firmware/src/utils.rs
+++ firmware/src/utils.rs
@@ -8,7 +8,7 @@ use ratatui_fretboard::note::{Note, STANDARD_TUNING};
 ///
 /// This functions only supports the standard tuning of a
 /// 6-string guitar as of now.
-pub fn generate_random_note(frets: RangeInclusive<u8>) -> Note {
+pub fn generate_random_note(frets: &RangeInclusive<u8>) -> Note {
     let fret = fastrand::u8(*frets.start()..=*frets.end());
     let string_index = fastrand::usize(..STANDARD_TUNING.len());
     STANDARD_TUNING[string_index].clone() + fret
